ba103a8a9c14e396ad5bca99f62606b351d9c4c1
Gogy <thegogy@proton.me>
2024-04-17 18:54:14 +0100

    Added basic CLI

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
+#include <complex.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <signal.h>
 #include <ncurses.h>
+#include <stdlib.h>
 
 #include "arrayutils.h"
 #include "renderer.h"
@@ -13,6 +16,21 @@ struct winsize w;
 
 int main(int argc, char **argv){
 
+  if (argc != 2) {
+    printf("Usage: %s <sorting algorithm>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  int algorithm;
+  if (strcmp("bogosort", argv[1]) == 0){
+    algorithm = 1;
+  } else if (strcmp("bubblesort", argv[1]) == 0) {
+    algorithm = 2;
+  } else if (strcmp("quicksort", argv[1]) == 0) {
+    algorithm = 3;
+  } else {
+    printf("Unknown sorting algorithm!\n");
+    return EXIT_FAILURE;
+  }
 
   struct WinSize ws = setupRender();
 
@@ -30,12 +48,15 @@ int main(int argc, char **argv){
   // Set up signal handler for Ctrl+C
   signal(SIGINT, handleExit);
   
-  // bogoSort(arr, ws.cols, ws.rows);
-  // bubbleSort(arr, ws.cols, ws.rows);
-  quickSort(arr, 0, ws.cols - 1, ws.cols, ws.rows);
-  getch();
+  if (algorithm == 1){
+    bogoSort(arr, ws.cols, ws.rows);
+  } else if (algorithm == 2) {
+    bubbleSort(arr, ws.cols, ws.rows);
+  } else if (algorithm == 3) {
+    quickSort(arr, 0, ws.cols - 1, ws.cols, ws.rows);
+  }
 
+  getch(); // Wait for user input before exiting
   handleExit(0);
-
-  return 0;
+  return EXIT_SUCCESS;
 }
