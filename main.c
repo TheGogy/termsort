adc279a20fe582d6f87a912463b7a24b2092ca8d
Gogy <thegogy@proton.me>
2024-04-17 19:12:22 +0100

    made cli more expandable

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <complex.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
@@ -12,6 +13,8 @@
 
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
+const char *algorithms[] = {"bogosort", "bubblesort", "quicksort"};
+const int n_algorithms = 3;
 struct winsize w;
 
 int main(int argc, char **argv){
@@ -20,15 +23,24 @@ int main(int argc, char **argv){
     printf("Usage: %s <sorting algorithm>\n", argv[0]);
     return EXIT_FAILURE;
   }
-  int algorithm;
-  if (strcmp("bogosort", argv[1]) == 0){
-    algorithm = 1;
-  } else if (strcmp("bubblesort", argv[1]) == 0) {
-    algorithm = 2;
-  } else if (strcmp("quicksort", argv[1]) == 0) {
-    algorithm = 3;
-  } else {
-    printf("Unknown sorting algorithm!\n");
+
+  if (strcmp("list", argv[1]) == 0) {
+    printf("Possible sorting algorithms: \n");
+    for (int i = 0; i < n_algorithms; i++) {
+      printf("%s\n", algorithms[i]);
+    }
+    return EXIT_SUCCESS;
+  }
+  
+  int algorithm = -1;
+  for (int i = 0; i < n_algorithms; i++) {
+    if(strcmp(algorithms[i], argv[1]) == 0){
+      algorithm = i;
+      break;
+    }
+  }
+  if (algorithm == -1) {
+    printf("\"%s\" is not a known sorting algorithm. To list available algorithms, use:\n\n%s list\n", argv[1], argv[0]);
     return EXIT_FAILURE;
   }
 
@@ -48,11 +60,11 @@ int main(int argc, char **argv){
   // Set up signal handler for Ctrl+C
   signal(SIGINT, handleExit);
   
-  if (algorithm == 1){
+  if (algorithm == 0){
     bogoSort(arr, ws.cols, ws.rows);
-  } else if (algorithm == 2) {
+  } else if (algorithm == 1) {
     bubbleSort(arr, ws.cols, ws.rows);
-  } else if (algorithm == 3) {
+  } else if (algorithm == 2) {
     quickSort(arr, 0, ws.cols - 1, ws.cols, ws.rows);
   }
 
